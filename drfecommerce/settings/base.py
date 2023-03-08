dea04c9d2df585c56b82eac313aac97cec336869
lorens <lorens32645@gmail.com>
2023-03-08 17:21:00 +0300

    changed secret key

--- drfecommerce/settings/base.py
+++ drfecommerce/settings/base.py
@@ -20,9 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = (
-    "django-insecure-^lxgna6#-#0vg4=!+zg7%+oucev+da=pv=6+to%238i&yyyx=z"
-)
+SECRET_KEY = "m0zl&+r%jyb0$6l+7@v)qaclmgq2awf^4_cj1b5=qm&uwvk5$c"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
