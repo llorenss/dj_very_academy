d696fe4ffe714193f24eebfa2254585095f4f3b2
lorens <lorens32645@gmail.com>
2023-03-08 22:38:11 +0300

    mptt

--- drfecommerce/product/models.py
+++ drfecommerce/product/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class Brand(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    is_digital = models.BooleanField(default=False)
