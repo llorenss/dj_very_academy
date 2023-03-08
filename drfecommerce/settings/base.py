6142aedbaa26563a37e3485c3c487ed2f716ff38
lorens <lorens32645@gmail.com>
2023-03-08 17:43:38 +0300

    python-dotenv for SECRET_KEY

--- drfecommerce/settings/base.py
+++ drfecommerce/settings/base.py
@@ -11,16 +11,22 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "m0zl&+r%jyb0$6l+7@v)qaclmgq2awf^4_cj1b5=qm&uwvk5$c"
+# SECRET_KEY = "m0zl&+r%jyb0$6l+7@v)qaclmgq2awf^4_cj1b5=qm&uwvk5$c"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
